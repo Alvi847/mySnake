a8aeac9bbc794941bb3f1a3004ca738d05465f22
Alvi847 <alcepeda@ucm.es>
2026-04-07 20:46:16 +0200

    MAS INFO SOBRE EL COMMIT ANTERIOR
    Cambién el sistema de coordenadas del propio tabllero (las globales, no las de dentro del tablero) para poder dibujarlo en distintas zonas de la pantalla y con distintos tamaños
    
    Creé la clase InputMappings, que guarda en un unordered_map los controles para interactuar con el juego. De esta forma, las serpientes se pueden mover por separado usando teclas distintas
    Los controles actuales son:
     WASD para una serpiente y las flechas de dirección para la otra

--- src/SnakeBody.cpp
+++ src/SnakeBody.cpp
@@ -7,9 +7,6 @@ void SnakeBody::update(float deltaTime){
 }
 
 void SnakeBody::emplace(Vector2 newPos){    
-    //std::cout << "BODY_POSITION_X: " << position.x << " BODY_POSITION_Y: " << position.y << std::endl;
-    //std::cout << "GOAL_X: " << lastSnakeBody->getLastPos().x << " GOAL_Y: " << lastSnakeBody->getLastPos().y << std::endl;
-
     Reptile::emplace(newPos);
 
     hasMoved = true;
